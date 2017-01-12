5874a729962589fcbff6efc1d35b3dd0bc6e185a
Nathan Hoad <nathan@getoffmalawn.com>
2017-01-12 17:49:12 +1000

    Version bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 
 setup(name='gbulb',
-      version='0.4',
+      version='0.5',
       description='GLib event loop for tulip (PEP 3156)',
       author='Nathan Hoad',
       author_email='nathan@getoffmalawn.com',
