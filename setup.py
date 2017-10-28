fa3d8c50dd37cb962e960a0e9dcf517699f520d2
Nathan Hoad <nathan@getoffmalawn.com>
2017-10-28 10:52:26 +1000

    Reformat this.

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ except ImportError:
     from distutils.core import setup
 
 
-setup(name='gbulb',
-      version='0.5.3',
-      description='GLib event loop for tulip (PEP 3156)',
-      author='Nathan Hoad',
-      author_email='nathan@getoffmalawn.com',
-      license='Apache 2.0',
-      url='http://github.com/nathan-hoad/gbulb',
-      packages=['gbulb'],
-      long_description="""Gbulb is a python library that implements a PEP 3156 interface for the GLib main event loop. It is designed to be used together with the tulip reference implementation.""",
-      classifiers=[
+setup(
+    name='gbulb',
+    version='0.5.3',
+    description='GLib event loop for tulip (PEP 3156)',
+    author='Nathan Hoad',
+    author_email='nathan@getoffmalawn.com',
+    license='Apache 2.0',
+    url='http://github.com/nathan-hoad/gbulb',
+    packages=['gbulb'],
+    long_description="""Gbulb is a python library that implements a PEP 3156 interface for the GLib main event loop. It is designed to be used together with the tulip reference implementation.""",
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
@@ -24,6 +25,5 @@ setup(name='gbulb',
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ]
+    ]
 )
-
